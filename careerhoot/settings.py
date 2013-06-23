c6d97f9c9ee8c0f60a8c300835745283ede37a5b
Andrew Chen <sirchen@gmail.com>
2013-06-23 06:03:59 -0700

    introkick heroku deploy - 13

--- careerhoot/settings.py
+++ careerhoot/settings.py
@@ -61,14 +61,14 @@ STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/introkick/static/'
+STATIC_URL = 'http://www.introkick.com/introkick/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/introkick/static/',
+    '/Users/anchen/Desktop/Hacking/django_projects/careerhoot/introkick/static/',
 )
 
 # List of finder classes that know how to find static files in

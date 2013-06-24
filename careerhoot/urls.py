e45c619879eacec8cc77a05cbc03ab76e51e5083
Andrew Chen <sirchen@gmail.com>
2013-06-23 19:31:09 -0700

    introkick heroku deploy - 34

--- careerhoot/urls.py
+++ careerhoot/urls.py
@@ -2,7 +2,7 @@
 
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from django.conf import settings
+import from careerhoot import settings
 import socket
 
 # from django.contrib.auth import views

57c242430c10a2e66551d03167a95a893d5fc63d
Andrew Chen <sirchen@gmail.com>
2013-06-23 19:11:25 -0700

    introkick heroku deploy - 32

--- careerhoot/urls.py
+++ careerhoot/urls.py
@@ -3,6 +3,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
+import socket
+
 # from django.contrib.auth import views
 
 # from introkick.views import twitter_login, twitter_logout, twitter_authenticated
@@ -30,6 +32,8 @@ urlpatterns = patterns('',
 	# url(r'^introkick/logout/$', views.logout, {'next_page': '/introkick'}, name='logout'),
 )
 
-urlpatterns += patterns('',
-    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
-)
+
+if socket.gethostname() != 'anchen-mn1':
+    urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )

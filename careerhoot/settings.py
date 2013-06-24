beb97a41f7b55b54db1d8853566a1c4c405c840d
Andrew Chen <sirchen@gmail.com>
2013-06-23 17:19:04 -0700

    introkick heroku deploy - 30

--- careerhoot/settings.py
+++ careerhoot/settings.py
@@ -25,7 +25,7 @@ DATABASES = {
 }
 
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config(default='postgres://andrewchen:ar468187postgres@localhost/introkick_db')
+DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')

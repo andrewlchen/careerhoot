1ccb22eae8f652e29589fa942263b6e9a426727b
Andrew Chen <sirchen@gmail.com>
2013-06-23 19:21:14 -0700

    introkick heroku deploy - 33

--- careerhoot/settings.py
+++ careerhoot/settings.py
@@ -73,7 +73,13 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+
+
+SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
+UP_ROOT = os.path.abspath(os.path.join(SITE_ROOT, '..'))
+
+
+STATIC_ROOT = UP_ROOT + '/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -85,7 +91,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(os.path.abspath(os.path.dirname(__file__) + '/..'), 'static'),
+    UP_ROOT + '/static/',
 )
 
 # List of finder classes that know how to find static files in

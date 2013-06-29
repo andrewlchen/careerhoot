0d803c7bee80277dc9dbf54e60e5f33cde52a1b1
Andrew Chen <sirchen@gmail.com>
2013-06-29 05:50:22 -0700

    github push - 13

--- careerhoot/urls.py
+++ careerhoot/urls.py
@@ -2,7 +2,7 @@
 
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from django.conf import settings
+from introkick.settings import *
 
 # from django.contrib.auth import views
 

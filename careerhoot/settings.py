c1b387672bdd6c967c868d662b0ca701eb14c5db
Andrew Chen <sirchen@gmail.com>
2013-06-29 04:58:52 -0700

    github push - 13

--- careerhoot/settings.py
+++ careerhoot/settings.py
@@ -84,6 +84,7 @@ STATIC_ROOT = UP_ROOT + '/introkick/static/'
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
 STATIC_URL = 'http://introkick.com/static/'
+ADMIN_MEDIA_PREFIX = 'http://introkick.com/static/admin/'
 
 
 # Additional locations of static files

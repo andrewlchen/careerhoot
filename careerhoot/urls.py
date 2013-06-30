0567b6ffeeef6ca59f694e2b18611c2766e61bfa
Andrew Chen <sirchen@gmail.com>
2013-06-30 16:24:49 -0700

    github push - 14

--- careerhoot/urls.py
+++ careerhoot/urls.py
@@ -33,5 +33,5 @@ urlpatterns = patterns('',
 
 
 urlpatterns += patterns('',
-    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': '/tmp/pip-build-root/django/django/contrib/admin/static/'}),
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 )

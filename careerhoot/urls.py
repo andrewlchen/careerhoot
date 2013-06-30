ea592da563e3d530582b107f8e48b44034d8d0fe
Andrew Chen <sirchen@gmail.com>
2013-06-29 17:04:04 -0700

    github push - 14

--- careerhoot/urls.py
+++ careerhoot/urls.py
@@ -33,5 +33,5 @@ urlpatterns = patterns('',
 
 
 urlpatterns += patterns('',
-    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': '/home/ubuntu/careerhoot/introkick/static/'}),
 )

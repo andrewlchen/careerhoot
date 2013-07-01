c23bd243dd7631d649cff23608198faa082c6be1
Andrew Chen <sirchen@gmail.com>
2013-07-01 00:00:08 -0700

    github push - 14

--- careerhoot/urls.py
+++ careerhoot/urls.py
@@ -3,6 +3,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 # from django.contrib.auth import views
@@ -36,3 +37,5 @@ urlpatterns = patterns('',
 urlpatterns += patterns('',
     (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 )
+
+urlpatterns += staticfiles_urlpatterns()

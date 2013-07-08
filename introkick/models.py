950616147e88fb4223cb007b9e943bb668067bf9
Andrew Chen <sirchen@gmail.com>
2013-07-08 00:14:29 -0700

    github push - paypal bug fixes - 22

--- introkick/models.py
+++ introkick/models.py
@@ -102,9 +102,7 @@ def recur_sub(sender, **kwargs):
     ipn_obj = sender
     affected_user = UserProfile.objects.get(user=User.objects.get(username=ipn_obj.custom))
     affected_user.subs_expiry += relativedelta(months=1)
-
     if affected_user.paid == False: 
 		affected_user.paid = True
-
 	affected_user.save()
 payment_was_successful.connect(recur_sub) 
